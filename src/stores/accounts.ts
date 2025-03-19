2bf87b01a8709e52801e381e4b9629b6de2bd5d2
vlad <vnelipovich63@gmail.com>
2025-03-19 16:46:19 +0300

    feat: add accounts store

---
+++ src/stores/accounts.ts
@@ -0,0 +1,34 @@
+import { defineStore } from 'pinia'
+
+export type Account = {
+  label: string
+  labelArray: { text: string }[]
+  type: 'LDAP' | 'Локальная'
+  login: string
+  password: string | null
+  errors?: { [key: string]: string }
+}
+
+export const useAccountsStore = defineStore('accounts', {
+  state: () => ({
+    accounts: [] as Account[],
+  }),
+  actions: {
+    addAccount() {
+      this.accounts.push({
+        label: '',
+        labelArray: [],
+        type: 'Локальная',
+        login: '',
+        password: '',
+        errors: {}
+      })
+    },
+    removeAccount(index: number) {
+      this.accounts.splice(index, 1)
+    },
+    updateAccount(index: number, account: Account) {
+      this.accounts[index] = account
+    }
+  },
+})
